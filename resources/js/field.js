f221f3f5087fd1e713827064d22eecfa3692f594
Evgeny Tronchuk <evgeny@tronchuk.com>
2022-04-06 20:13:53 +0300

    Upgrade to work with Nova 4.0
    (cherry picked from commit dc7e17178c205963a426e79436f06827d10c7b40)
    
    # Conflicts:
    #	dist/js/field.js

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,26 +1,26 @@
-Nova.booting((Vue, router, store) => {
-    // Vue.config.devtools = true;
+Nova.booting((app, store) => {
+    // app.config.devtools = true;
 
-    Vue.filter('striphtml', function (value) {
-        var div = document.createElement("div");
-        div.innerHTML = value;
-        var text = div.textContent || div.innerText || "";
-        return text;
-    });
+    // app.filter('striphtml', function (value) {
+    //     var div = document.createElement("div");
+    //     div.innerHTML = value;
+    //     var text = div.textContent || div.innerText || "";
+    //     return text;
+    // });
 
-    Vue.component('index-nova-translatable', require('./components/IndexField'))
-    Vue.component('detail-nova-translatable', require('./components/DetailField'))
-    Vue.component('form-nova-translatable', require('./components/FormField'))
+    app.component('index-nova-translatable', require('./components/IndexField'))
+    app.component('detail-nova-translatable', require('./components/DetailField'))
+    app.component('form-nova-translatable', require('./components/FormField'))
 
-    Vue.component('index-nova-translatable-slug-field', require('./components/Slug/IndexField'))
-    Vue.component('detail-nova-translatable-slug-field', require('./components/Slug/DetailField'))
-    Vue.component('form-nova-translatable-slug-field', require('./components/Slug/FormField'))
+    app.component('index-nova-translatable-slug-field', require('./components/Slug/IndexField'))
+    app.component('detail-nova-translatable-slug-field', require('./components/Slug/DetailField'))
+    app.component('form-nova-translatable-slug-field', require('./components/Slug/FormField'))
 
-    Vue.component('index-nova-translatable-code-field', require('./components/Code/IndexField'))
-    Vue.component('detail-nova-translatable-code-field', require('./components/Code/DetailField'))
-    Vue.component('form-nova-translatable-code-field', require('./components/Code/FormField'))
+    app.component('index-nova-translatable-code-field', require('./components/Code/IndexField'))
+    app.component('detail-nova-translatable-code-field', require('./components/Code/DetailField'))
+    app.component('form-nova-translatable-code-field', require('./components/Code/FormField'))
 
-    Vue.component('index-nova-translatable-grapes-field', require('./components/Grapes/IndexField'))
-    Vue.component('detail-nova-translatable-grapes-field', require('./components/Grapes/DetailField'))
-    Vue.component('form-nova-translatable-grapes-field', require('./components/Grapes/FormField'))
+    app.component('index-nova-translatable-grapes-field', require('./components/Grapes/IndexField'))
+    app.component('detail-nova-translatable-grapes-field', require('./components/Grapes/DetailField'))
+    app.component('form-nova-translatable-grapes-field', require('./components/Grapes/FormField'))
 })
