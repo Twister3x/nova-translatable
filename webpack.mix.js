e413c6d5e37f76e8c79fe2473c8b727fd0cf38c1
Evgeny Tronchuk <evgeny@tronchuk.com>
2022-04-06 19:07:25 +0300

    Upgrade to work with Nova 4.0
    (cherry picked from commit 300554e6464a565386a239b205c7d11abbfd437b)
    
    # Conflicts:
    #	composer.json
    #	dist/js/field.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,14 @@
-let mix = require('laravel-mix')
+let mix = require('laravel-mix');
+let path = require('path');
+
+mix.alias({
+    'laravel-nova': path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/packages.js'),
+});
 
 mix
   .setPublicPath('dist')
-  .js('resources/js/field.js', 'js')
-  .sass('resources/sass/field.scss', 'css');
+  .js('resources/js/field.js', 'js').vue();
+
+mix.sass('resources/sass/field.scss', 'css');
 
 mix.copy('resources/Editor/editor-styles.css', 'dist/css')
